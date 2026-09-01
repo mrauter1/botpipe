ec25721c12d3e93ba2c91c7cc48a2a4618692d7e
Marcelo Rauter <marcelorauter@gmail.com>
2026-08-31 22:08:22 -0300

    Export rubric judgment contracts

--- botpipe/workflows/adaptive_goal/__init__.py
+++ botpipe/workflows/adaptive_goal/__init__.py
@@ -1,27 +1,33 @@
 """Adaptive verifier-driven goal workflow."""
 
 from .contracts import (
-    AcceptanceRule,
     ActionCapabilityResult,
     AdaptiveGoalInput,
     AdaptiveGoalOutput,
     CapabilityRegistry,
     CapabilitySpec,
+    CriterionJudgment,
+    DeterministicRule,
     MissionCriterion,
     MissionSpec,
+    RubricFinding,
+    RubricItem,
     VerifierCapabilityResult,
 )
 from .flow import AdaptiveGoalWorkflow
 
 __all__ = [
-    "AcceptanceRule",
     "ActionCapabilityResult",
     "AdaptiveGoalInput",
     "AdaptiveGoalOutput",
     "AdaptiveGoalWorkflow",
     "CapabilityRegistry",
     "CapabilitySpec",
+    "CriterionJudgment",
+    "DeterministicRule",
     "MissionCriterion",
     "MissionSpec",
+    "RubricFinding",
+    "RubricItem",
     "VerifierCapabilityResult",
 ]
